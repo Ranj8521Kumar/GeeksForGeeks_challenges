332d940b499e77f5f36f177cbdf9ee6a7154ae92
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-11-25 23:56:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+        int n = arr.size();
+        if(n==0) return 0;
+        
+        int maxProduct = arr[0];
+        int minProduct = arr[0];
+        int result = arr[0];
+        
+        for(int i = 1; i<n; i++){
+            int it = arr[i];
+            if(it<0){
+                swap(maxProduct,minProduct);
+            }
+            
+            maxProduct = max(it, maxProduct*it);
+            minProduct = min(it, minProduct*it);
+            
+            result = max(result, maxProduct);
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
