503cd1e111c672cb92b1f15950b31300badc7f81
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-18 21:23:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.cpp
+++ Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.cpp
@@ -4,41 +4,49 @@ using namespace std;
 
 
 // } Driver Code Ends
-
 class Solution {
   public:
-    //function for isCycleDFS
-    bool isCycleDFS(vector<vector<int>>& adj, int u,  vector<bool>& visited, vector<bool>& recVisited){
-        visited[u] = true;
-        recVisited[u] = true;
-        
-        for(auto &v: adj[u]){
-            if(visited[v] && recVisited[v]){
-                return true;
-            }else{
-                if(!visited[v]){
-                    if(isCycleDFS(adj, v, visited, recVisited)){
-                        return true;
-                    }
-                }
-            }
-        }
-        recVisited[u] = false;
-        return false;
-    }
-    
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<vector<int>> adj) {
         // code here
+        
+        //using khan's algorithm or using bfs or using topological sort
+        
         int v = adj.size();
         
-        vector<bool> visited(v, false);
-        vector<bool> recVisited(v, false);
+        vector<int> indegree(v, 0);
         
-        for(int u = 0;  u<v; u++){
-            if(!visited[u] && isCycleDFS(adj, u, visited, recVisited)){
-                return true;
+        for(int u = 0; u<v; u++){
+            for(auto &v: adj[u]){
+                indegree[v]++;
             }
+        }
+        
+        queue<int> que;
+        for(int u = 0; u<v; u++){
+            if(indegree[u] == 0){
+                que.push(u);
+            }
+        }
+        
+        int count = 0;
+        
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            
+            count++;
+            
+            for(auto &v: adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    que.push(v);
+                }
+            }
+        }
+        
+        if(count !=  v){
+            return true;
         }
         
         return false;
