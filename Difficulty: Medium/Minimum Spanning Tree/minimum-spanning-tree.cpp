c1f5d187f5fbc49ae7578e0184ba56a5916c91a8
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-23 08:53:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 class Solution {
   public:
-    typedef pair<int, pair<int, int>> p;
+  //using prims Algoritm
+    typedef pair<int, int> p;
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int v, vector<vector<int>> adj[]) {
         // code here
         vector<bool> visited(v, false);
-        vector<int> parent(v, -1);
         
         priority_queue<p, vector<p>, greater<p>> pq;
-        pq.push({0, {0, -1}}); //{wt, {node, from}}
+        pq.push({0, 0}); //{wt, node}
         
         int result = 0;
         
@@ -23,14 +23,12 @@ class Solution {
             auto u = pq.top();
             pq.pop();
             
-            int includedNodeWithMST = u.second.first;
-            int from = u.second.second;
+            int includedNodeWithMST = u.second;
             int wt = u.first;
             
             if(visited[includedNodeWithMST]) continue; //already include in MSt
             
             visited[includedNodeWithMST] = true;
-            parent[includedNodeWithMST] = from;
             result += wt;
             
             for(auto &v: adj[includedNodeWithMST]){
@@ -38,7 +36,7 @@ class Solution {
                 int wt = v[1];
                 
                 if(!visited[node]){
-                    pq.push({wt, {node, includedNodeWithMST}});
+                    pq.push({wt, node});
                 }
             }
         }
