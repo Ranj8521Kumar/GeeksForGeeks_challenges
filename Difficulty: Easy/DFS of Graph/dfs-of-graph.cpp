a97552e8481092c0f054eb169df5f4c549932d27
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-19 16:24:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,70 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    //function for  dfs trravesal
-    void DFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<int>& result){
-        if(visited[u] == true)  return;
+    void dfsTravesal(vector<vector<int>>& adj, vector<int> &result, vector<int> &visited, int u){
+        if(visited[u] == true) return;
         
         result.push_back(u);
         visited[u] = true;
         
         for(auto &v: adj[u]){
             if(!visited[v]){
-                DFS(adj, v, visited, result);
+                dfsTravesal(adj, result, visited, v);
             }
+            
         }
     }
   
-    // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
+    vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        int v = adj.size(); //number of vertices
-        
-        int u = 0;
-        
+        int v = adj.size();
         vector<int> result;
         
-        vector<bool> visited(v, false);
+        vector<int> visited(v, false);
         
-        DFS(adj, u, visited, result);
+        dfsTravesal(adj, result, visited, 0);
         
         return result;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<vector<int>> adj(
-            V); // Use vector of vectors instead of array of vectors.
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        Solution obj;
-        vector<int> ans = obj.dfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
