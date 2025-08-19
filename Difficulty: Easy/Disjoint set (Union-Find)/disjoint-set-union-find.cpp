33c079988a0655f4a98693e11f0aba9d56d420bf
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-20 01:53:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
+++ Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
@@ -1,57 +1,19 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-int find(int a[],int x);
-void unionSet(int a[],int x ,int z);
-int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		int a[n+1];
-		for(int i=1;i<=n;i++)
-			a[i]=i;
-		int k;
-		cin>>k;
-		for(int i=0;i<k;i++){
-			string s;
-			cin>>s;
-			if(s=="UNION"){
-				int z,x;
-				cin>>x>>z;
-				unionSet(a,x,z);
-			}
-			else{
-				int x;
-				cin>>x;
-				int parent=find(a,x);
-				cout<<parent<<" ";
-			}
-		}
-		cout<<endl;
-	
-cout << "~" << "\n";
-}
-}
-// } Driver Code Ends
-
-int find(int A[], int x) {
-    // Path compression: make every node point directly to the root
-    if (A[x] != x) {
-        A[x] = find(A, A[x]); // Recursively find the root and compress the path
+/*Complete the functions below*/
+int find(int par[], int x) {
+    // add code here
+    if(x == par[x]){
+        return x;
     }
-    return A[x];
+    
+    return find(par, par[x]);
 }
 
-void unionSet(int A[], int x, int z) {
-    // Find roots of x and z
-    int x_parent = find(A, x);
-    int z_parent = find(A, z);
-
-    // If they are not already in the same set
-    if (x_parent != z_parent) {
-        // Union: attach one root to the other
-        A[x_parent] = z_parent;
+void unionSet(int par[], int x, int y) {
+    // add code here.
+    int x_parent = find(par, x);
+    int y_parent = find(par, y);
+    
+    if(x_parent != y_parent){
+        par[x_parent] = y_parent;
     }
 }
