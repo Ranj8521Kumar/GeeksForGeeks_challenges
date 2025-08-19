b54c6815c3bdf76229119906d591ee8ca3ba1be0
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-20 01:19:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,84 +1,46 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-public:
-    bool BFS(vector<vector<int>>& adj, int u, vector<int>& color, int currColor){
-        queue<int> que;
-        que.push(u);
-
-        color[u] = currColor;
-
-        while(!que.empty()){
-            int u = que.front();
-            que.pop();
-            
-            for(auto &v: adj[u]){
-                if(color[v] == color[u]){
-                    return false;
-                }
+  public:
+    bool DFS(unordered_map<int, vector<int>>& adj, vector<int>& color, int currNode, int currColor){
+        color[currNode] = currColor;
         
-                if(color[v] == -1){
-                    que.push(v);
-                    if(color[u] == 1){
-                        color[v] = 0;
-                    }else{
-                        color[v] = 1; 
-                    }
+        for(auto &v: adj[currNode]){
+            if(color[v] == -1){
+                currColor = 1 - color[currNode];
+                
+                if(DFS(adj, color, v, currColor) == false){
+                    return false;
                 }
-            }
-        }
-
-        return true;
-    }
-
-    bool isBipartite(vector<vector<int>>& graph) {
-        //using bfs
-        int v  = graph.size();
-
-        vector<int> color(v, -1);
-
-        for(int u = 0; u<v; u++){
-            if(color[u] == -1){
-                int currColor = 1;
-                if(!BFS(graph, u, color, currColor)){
+            }else{
+                if(color[v] == color[currNode]){
                     return false;
                 }
             }
         }
-
+        
         return true;
     }
-};
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj(V);
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
+  
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        unordered_map<int, vector<int>> adj;
+        
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        Solution obj;
-        bool ans = obj.isBipartite(adj);
-        if (ans)
-            cout << "true\n";
-        else
-            cout << "false\n";
-
-        cout << "~"
-             << "\n";
+        
+        vector<int> color(V, -1);
+        // 0 -> Red
+        // 1 -> Green
+        
+        for(int u = 0; u<V; u++){
+            if(color[u] == -1 && DFS(adj, color, u, 0) == false){
+                return false;
+            }
+        }
+        
+        return true;
     }
-    return 0;
-}
-// } Driver Code Ends
+};
