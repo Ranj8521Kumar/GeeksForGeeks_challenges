db32268ad22872b6a380e8850ae20978669bbf52
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-21 00:26:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,80 +1,43 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-  //using prims Algoritm
-    typedef pair<int, int> p;
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int v, vector<vector<int>> adj[]) {
-        // code here
-        vector<bool> visited(v, false);
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        unordered_map<int, vector<pair<int, int>>> adj;
         
-        priority_queue<p, vector<p>, greater<p>> pq;
-        pq.push({0, 0}); //{wt, node}
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
         
-        int result = 0;
+        vector<bool> inMST(V, false);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        int sum = 0;
+        pq.push({0, 0});
+        sum += 0;
         
         while(!pq.empty()){
-            auto u = pq.top();
+            auto it = pq.top();
             pq.pop();
+            int weight = it.first;
+            int node = it.second;
             
-            int includedNodeWithMST = u.second;
-            int wt = u.first;
-            
-            if(visited[includedNodeWithMST]) continue; //already include in MSt
-            
-            visited[includedNodeWithMST] = true;
-            result += wt;
-            
-            for(auto &v: adj[includedNodeWithMST]){
-                int node = v[0];
-                int wt = v[1];
-                
-                if(!visited[node]){
-                    pq.push({wt, node});
-                }
+            if(inMST[node] != true){
+                sum += weight;
+                inMST[node] = true;
+                for(auto &temp: adj[node]){
+                    int v = temp.first;
+                    int w = temp.second;
+                    
+                    pq.push({w, v});
+                }  
             }
         }
         
-        return result;
+        return sum;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i = 0;
-        while (i++ < E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            vector<int> t1, t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
-        }
-
-        Solution obj;
-        cout << obj.spanningTree(V, adj) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
