847dd3806166631b5df5a2d26516dee5ee0ca1bd
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-20 23:37:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -1,91 +1,45 @@
-//{ Driver Code Starts
-#include <cstdio> // for freopen
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
+
 class Solution {
   public:
-    /*  Function to implement Bellman Ford
-     *   edges: vector of vectors which represents the graph
-     *   src: source vertex
-     *   V: number of vertices
-     */
-    vector<int> bellmanFord(int v, vector<vector<int>>& edges, int src) {
-        // Code here
-        vector<int> distance(v, 100000000);
-        distance[src] = 0;
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
         
-        for(int relax = 1; relax<=v-1; relax++){
+        for(int i = 0; i<V-1; i++){
             for(auto &edge: edges){
                 int u = edge[0];
                 int v = edge[1];
-                int wt = edge[2];
+                int w = edge[2];
                 
-                if(distance[u] != 100000000 && distance[u] + wt < distance[v]){
-                    distance[v] = distance[u] + wt;
+                if(dist[u] != INT_MAX){
+                    if(w + dist[u] < dist[v]){
+                        dist[v] = w + dist[u];
+                    }
                 }
             }
         }
         
-            for(auto &edge: edges){
-                int u = edge[0];
-                int v = edge[1];
-                int wt = edge[2];
+        // For detecting -ve cycle
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
                 
-                if(distance[u] != 100000000 && distance[u] + wt < distance[v]){
+            if(dist[u] != INT_MAX){
+                if(w + dist[u] < dist[v]){
                     return {-1};
                 }
             }
+        }
         
-        return distance;
+        for(int i = 0; i<V; i++){
+            if(dist[i] == INT_MAX){
+                dist[i] = 1e8;
+            }
+        }
+            
+        
+        return dist;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int N, m;
-        cin >> N >> m;
-
-        vector<vector<int> > edges;
-
-        for (int i = 0; i < m; ++i) {
-            int u, v, w;
-            cin >> u >> v >> w;
-
-            vector<int> edge(3);
-            edge[0] = u;
-            edge[1] = v;
-            edge[2] = w;
-            edges.push_back(edge);
-        }
-
-        int src;
-        cin >> src;
-        cin.ignore();
-
-        Solution obj;
-        vector<int> res = obj.bellmanFord(N, edges, src);
-
-        for (size_t i = 0; i < res.size(); i++) {
-            cout << res[i] << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
