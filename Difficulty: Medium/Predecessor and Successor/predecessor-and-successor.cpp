ac2a36870e9a1de5250641b5a9ceceffbf620349
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-18 16:51:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -1,6 +1,5 @@
 /* BST Node
-class Node
-{
+class Node {
    public:
     int data;
     Node *left;
@@ -11,43 +10,54 @@ class Node
         left = NULL;
         right = NULL;
     }
-}; */
+};
+*/
 
 class Solution {
   public:
-    void inOrderTraversal(Node* root, vector<Node*> &inorder) {
-        if(root == nullptr) return;
-
-        inOrderTraversal(root->left, inorder);
+    void traverse(Node* root, vector<Node*>& inorder){
+        if(!root) return;
+        
+        traverse(root->left, inorder);
         inorder.push_back(root);
-        inOrderTraversal(root->right, inorder);
+        traverse(root->right, inorder);
     }
-
+  
     vector<Node*> findPreSuc(Node* root, int key) {
+        // code here
         vector<Node*> inorder;
-        inOrderTraversal(root, inorder);
+        traverse(root, inorder);
+        
+        Node* pre = nullptr;
+        Node* suc = nullptr;
         int n = inorder.size();
-        vector<Node*> result(2, nullptr); // {predecessor, successor}
-
-        // If key exists in tree
-        for (int i = 0; i < n; ++i) {
-            if (inorder[i]->data == key) {
-                if (i > 0) result[0] = inorder[i - 1];
-                if (i < n - 1) result[1] = inorder[i + 1];
-                return result;
+        int low = 0;
+        int high = n-1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(inorder[mid]->data < key){
+                pre = inorder[mid];
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
-
-        // Key doesn't exist, use lower_bound
-        auto lb = lower_bound(inorder.begin(), inorder.end(), key, [](Node* a, int val) {
-            return a->data < val;
-        });
-
-        int idx = lb - inorder.begin();
-
-        if (idx > 0) result[0] = inorder[idx - 1]; // predecessor
-        if (idx < n) result[1] = inorder[idx];     // successor
-
-        return result;
+        
+        low = 0;
+        high = n-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(inorder[mid]->data > key){
+                suc = inorder[mid];
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        
+        return {pre, suc};
     }
 };
